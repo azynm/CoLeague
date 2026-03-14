c3ac49a31c7a34a06c477fcc44ae644a69fdff64
Aadi Deepchand <aadideepchand8254@gmail.com>
2026-03-14 13:33:08 +0000

    Added basic Flask app setup

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 #Main entry point for frontend 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello, World!</h1><p>My Flask app is alive.</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
